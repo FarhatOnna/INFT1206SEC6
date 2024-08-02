2abc3a33bd0b1b0be88b49615a38b4d81e1fc3c4
FarhatOnna <farhat.rahman@dcmail.ca>
2024-08-01 23:09:27 -0400

    Assignment4part2
    declared the array of image filenames

--- Assignments/Assignment4/Part2/main.js
+++ Assignments/Assignment4/Part2/main.js
@@ -5,7 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const image = [`pic1.jpg`,`pic2.jpg`,`pic3.jpg`,`pic4.jpg`,`pic5.jpg`];
+const alts = {
+    'pic1.jpg' : 'closeup of a blue human eye',
+    'pic2.jpg' : 'photo of a rock',
+    'pic3.jpg' : 'purple and white flower',
+    'pic4.jpg' : 'picture of a tomb',
+    'pic5.jpg' : 'Butterfly on a leaf'
+}
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */

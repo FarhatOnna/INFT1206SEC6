00fea88c6727f4c057fd6bf30da0dbda8eb98bdd
FarhatOnna <farhat.rahman@dcmail.ca>
2024-08-01 23:58:21 -0400

    Assignment4part2
    adding the for loop

--- Assignments/Assignment4/Part2/main.js
+++ Assignments/Assignment4/Part2/main.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const image = [`pic1.jpg`,`pic2.jpg`,`pic3.jpg`,`pic4.jpg`,`pic5.jpg`];
+const images = [`pic1.jpg`,`pic2.jpg`,`pic3.jpg`,`pic4.jpg`,`pic5.jpg`];
 const alts = {
     'pic1.jpg' : 'closeup of a blue human eye',
     'pic2.jpg' : 'photo of a rock',
@@ -13,13 +13,22 @@ const alts = {
     'pic4.jpg' : 'picture of a tomb',
     'pic5.jpg' : 'Butterfly on a leaf'
 }
+
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (const image of images){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/${image}');
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e =>{
+        displayedImage.src = e.target.src;
+        displayedImage.src = e.target.alt;
+    });
+
+}
+
 
 /* Wiring up the Darken/Lighten button */
